44752b6881af4a7c8468c3c6ff4bf7f20b25467a
DaniilSukhanov <82699248+DaniilSukhanov@users.noreply.github.com>
2024-10-14 21:20:46 +0300

    Homework

--- AutumnSchool24/LoginScene/EnterViewController.swift
+++ AutumnSchool24/LoginScene/EnterViewController.swift
@@ -6,11 +6,181 @@
 //
 
 import UIKit
+import OSLog
 
 final class EnterViewController: UIViewController {
-    struct Constants {
+    private let logger = Logger(subsystem: "AutmnSchool24", category: "EnterViewController")
+    private let patternPassword = "^(?=.+[A-Z])(?=.+[0-9])(?=.+[.,?!():;]).{8,}$"
+    private struct Constants {
         static let cornerRadiusTextField: CGFloat = 15
         static let cornerRadiusButton: CGFloat = 10
         static let customSpacing: CGFloat = 40
+        static let leadingInset: CGFloat = 22
+    }
+    
+    private lazy var loginField: UITextField = {
+        let field = UITextField()
+        field.layer.masksToBounds = true
+        field.borderStyle = .roundedRect
+        field.layer.cornerRadius = Constants.cornerRadiusTextField
+        field.placeholder = "Login"
+        return field
+    }()
+    
+    private lazy var passwordField: UITextField = {
+        let field = UITextField()
+        field.placeholder = "Password"
+        field.layer.masksToBounds = true
+        field.borderStyle = .roundedRect
+        field.layer.cornerRadius = Constants.cornerRadiusTextField
+        field.addTarget(self, action: #selector(handlePasswordChange), for: .editingChanged)
+        return field
+    }()
+    
+    private lazy var loginTitle: UILabel = {
+        let label = UILabel()
+        label.text = "Enter your login and password"
+        label.textAlignment = .center
+        label.textColor = .styleruSubtitle
+        return label
+    }()
+    
+    private lazy var mainStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.spacing = Constants.customSpacing
+        return stackView
+    }()
+    
+    private lazy var loginButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Login", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.setTitleColor(.styleruGray, for: .highlighted)
+        button.layer.cornerRadius = Constants.cornerRadiusButton
+        button.addTarget(self, action: #selector(moveNextView), for: .touchUpInside)
+        button.layer.borderWidth = 1
+        button.layer.borderColor = UIColor.styleruBorderGray.cgColor
+        button.backgroundColor = .styleruLightGray
+        return button
+    }()
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+}
+
+// MARK: - Handlers
+
+private extension EnterViewController {
+    @objc func handlePasswordChange() {
+        guard let password = passwordField.text else {
+            return
+        }
+        if password.count >= 8 && checkPasswordCorrect(password) {
+            passwordField.layer.borderWidth = 0
+            passwordField.layer.borderColor = nil
+            
+        } else {
+            passwordField.layer.borderWidth = 1
+            passwordField.layer.borderColor = UIColor.red.cgColor
+        }
+    }
+}
+
+// MARK: - Actions
+
+private extension EnterViewController {
+    @objc func loginButtonTapped() {
+        if checkFields() {
+            logger.info("Registartion successful!")
+            let animationVC = EnterViewController()
+            navigationController?.pushViewController(animationVC, animated: true)
+        }
+    }
+}
+
+// MARK: - Check fields
+
+private extension EnterViewController {
+    func checkFields() -> Bool {
+        guard
+            let login = loginField.text,
+            let password = passwordField.text,
+            !login.isEmpty,
+            !password.isEmpty
+        else {
+            logger.info("Check fields not correct!")
+            return false
+        }
+        return checkPasswordCorrect(password)
+    }
+    
+    func checkPasswordCorrect(_ password: String) -> Bool {
+        password.range(of: patternPassword) != nil
+    }
+}
+
+// MARK: - Move next view
+    
+private extension EnterViewController {
+    @objc func moveNextView() {
+        print(1)
+        if checkFields() {
+            let animationVC = EnterViewController()
+            navigationController?.pushViewController(animationVC, animated: true)
+        }
+    }
+}
+
+// MARK: - SetupUI
+
+private extension EnterViewController {
+    func setupUIMainStackView() {
+        view.addSubview(mainStackView)
+        mainStackView.translatesAutoresizingMaskIntoConstraints = false
+        mainStackView.addArrangedSubview(loginField)
+        mainStackView.addArrangedSubview(passwordField)
+        
+        mainStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        mainStackView.topAnchor.constraint(equalTo: loginTitle.bottomAnchor, constant: 40).isActive = true
+        
+        mainStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: Constants.leadingInset).isActive = true
+    }
+    
+    func setupUILoginButton() {
+        view.addSubview(loginButton)
+        loginButton.translatesAutoresizingMaskIntoConstraints = false
+        loginButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        loginButton.widthAnchor.constraint(equalToConstant: 200).isActive = true
+        loginButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        loginButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -Constants.customSpacing).isActive = true
+    }
+    
+    func setupUILoginTitle() {
+        view.addSubview(loginTitle)
+        loginTitle.translatesAutoresizingMaskIntoConstraints = false
+        loginTitle.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        loginTitle.topAnchor.constraint(equalTo: view.topAnchor, constant: 70).isActive = true
+        loginTitle.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: Constants.leadingInset).isActive = true
+    }
+    
+    func setupUIView() {
+        view.backgroundColor = .black
+    }
+    
+    func setupUI() {
+        setupUIView()
+        setupUILoginButton()
+        setupUILoginTitle()
+        setupUIMainStackView()
+        
+    }
+}
+
+extension EnterViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
     }
 }
